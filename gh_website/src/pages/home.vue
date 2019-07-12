92ca6e84ae5dec3f936172ab8d5ebab32da2b276
anniZhe <feizhe0219@126.com>
2019-07-12 15:46:32 +0800

    web server init

---
+++ gh_website/src/pages/home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+      {{ data }}
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      data: '第一次上服务器'
+    }
+  }
+}
+
+</script>
+<style>
+  .home{
+    width: 100%;
+  }
+</style>
